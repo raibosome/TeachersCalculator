7074ab127f9a0a9765ea2dd46d2a9133c3946762
raibosome <raimi.bkarim@gmail.com>
2018-04-26 11:39:15 +0800

    Implement swipe to entire root stack view

--- TeachersCalculator/ViewController.swift
+++ TeachersCalculator/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var lastButton: UITextView!
     @IBOutlet weak var displayView: UILabel!
+    @IBOutlet weak var button: UIButton!
     
     var sum: Double = 0.0
     
@@ -18,26 +20,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(displayView.isUserInteractionEnabled)
+//        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPress(_:)))
+//        self.button.addGestureRecognizer(longPress)
+//
+        //print(displayView.isUserInteractionEnabled)
     }
     
     @IBAction func swipeHandler(_ sender: UISwipeGestureRecognizer) {
         if sender.state == .ended {
-            print("tada")
+            print("Swipe registered.")
         }
     }
     
-
+    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            print("True swipe registered.")
+        }
+    }
+    
     @IBAction func buttonTap(_ button: UIButton) {
         if (button.tag != -1) {
+            lastButton.text = String(button.tag)
             sum = sum + Double(button.tag)
             displayView.text = String(sum)
         } else {
             sum = 0.0
+            lastButton.text = "0"
             displayView.text = "0"
         }
     }
-    
+
+    @IBAction func trueLongPress(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            print("True long press registered.")
+        }
+    }
     
 }
 
