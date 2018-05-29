f7ffd5d1180676f6b4f69cf11f5a7bb6ff3df191
raibosome <raimi.bkarim@gmail.com>
2018-05-29 14:40:00 +0800

    Clean codes, bug fix on division by zero

--- TeachersCalculator/ViewController.swift
+++ TeachersCalculator/ViewController.swift
@@ -41,11 +41,12 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
         super.viewDidLoad()
         
         // Banner
-        bannerView.delegate = self
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-        bannerView.rootViewController = self
-        bannerView.load(GADRequest())
+        //        bannerView.delegate = self
+        //        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        //        bannerView.rootViewController = self
+        //        bannerView.load(GADRequest())
         
+        // Tap on displayView to switch mode
         let tap = UITapGestureRecognizer(target: self, action: #selector(displayViewTap))
         displayView.isUserInteractionEnabled = true
         displayView.addGestureRecognizer(tap)
@@ -54,7 +55,6 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
         let swipeleft = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft))
         swipeleft.direction = .left
         stackView.addGestureRecognizer(swipeleft)
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -62,23 +62,9 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
         showToast(controller: self, message: "Calculating cumulative sum", seconds: 1.0)
     }
     
-    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
-        if sender.direction == UISwipeGestureRecognizerDirection.right {
-            if adder <= 80 {
-                adder = adder + 10
-            }
-        } else {
-            if adder >= 10 {
-                adder = adder - 10
-            }
-        }
-        if sender.state == .ended {
-            setButtonTitles()
-        }
-    }
-    
     @IBAction func buttonTap(_ button: MyButton) {
-        if 0 ... 10 ~= button.tag {
+        switch button.tag {
+        case 0 ... 10:
             if inputs.count < 100 {
                 currentVal = button.tag == 0 ? 0.0 : Double(button.tag) + Double(adder)
                 inputs.append(Double(currentVal))
@@ -91,29 +77,26 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
                 updateHistory()
             }
             
-        // Back button
-        } else if button.tag == -2 {
+        case -2: // back button
             if inputs.count > 0 {
                 let lastInput = inputs.removeLast()
                 obs = lastInput.isNaN ? obs : obs - 1
+                print(obs)
                 sum = lastInput.isNaN ? sum : sum - lastInput
                 avg = sum/Double(obs)
                 
                 showDisplay()
                 removeLastLine()
             }
-        
-        // Copy history
-        } else if button.tag == -3 {
-            exportAsIs(historyView.text)
             
-        // Save file
-        } else if button.tag == -4 {
+        case -3: // Copy button
+            exportAsIs(historyView.text)
+
+        case -4: // CSV button
             let csvText = historyView.text.replacingOccurrences(of: ")\t", with: ",")
             exportAsCsv(csvHeader + csvText)
             
-        // NA button
-        } else if button.tag == -5 {
+        case -5: // NA button
             if inputs.count < 100 {
                 currentVal = Double.nan
                 inputs.append(Double(currentVal))
@@ -122,8 +105,7 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
                 updateHistory()
             }
             
-        // Clear button
-        } else if button.tag == -1 {
+        default: // C button
             sum = 0.0
             avg = 0.0
             currentVal = 0.0
@@ -137,7 +119,7 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
             showToast(controller: self, message: "Cumulative sum", seconds: 0.5)
         }
     }
-
+    
     @IBAction func trueLongPress(_ sender: UILongPressGestureRecognizer) {
         if sender.state == .began {
             if let buttonTag = sender.view?.tag {
@@ -163,12 +145,35 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
         }
     }
     
-    func numberToDisplay(_ value: Double) -> String {
-        if value.truncatingRemainder(dividingBy: 1) > 0 {
-            return String(format: "%.1f", value)
-        } else {
-            return String(format: "%.0f", value)
+    // Show sum or average
+    @objc func displayViewTap(sender: UITapGestureRecognizer) {
+        sumView = !sumView
+        showDisplay()
+        let message = sumView ? "Cumulative sum" : "Cumulative average"
+        showToast(controller: self, message: message, seconds: 0.5)
+    }
+    
+    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
+        if adder <= 80 && sender.direction == UISwipeGestureRecognizerDirection.right {
+            adder = adder + 10
         }
+        if sender.state == .ended {
+            setButtonTitles()
+        }
+    }
+    
+    @objc func swipeLeft(sender: UISwipeGestureRecognizer) {
+        if adder >= 10 && sender.direction == .left {
+            adder = adder - 10
+        }
+        if sender.state == .ended {
+            setButtonTitles()
+        }
+    }
+    
+    // Truncate to whole number whenever possible
+    func numberToDisplay(_ value: Double) -> String {
+        return value.truncatingRemainder(dividingBy: 1) > 0 ? String(format: "%.1f", value) : String(format: "%.0f", value)
     }
     
     func setButtonTitles() {
@@ -200,12 +205,22 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
     }
     
     func updateHistory() {
-        let currentLine = String(inputs.count) + ")\t" + numberToDisplay(inputs.last!)
+        let currentLine = (inputs.last?.isNaN)! ?
+            String(inputs.count) + ")" : String(inputs.count) + ")\t" + numberToDisplay(inputs.last!)
         if historyView.text != "" {
             historyView.text = historyView.text + "\n"
         }
         historyView.text = historyView.text + currentLine
         scrollTextViewToBottom(historyView)
+    }
+    
+    // Display type average or sum
+    func showDisplay() {
+        if !sumView && obs == 0 {
+            displayView.text = "-"
+        } else {
+            displayView.text = sumView ? numberToDisplay(sum) : numberToDisplay(avg)
+        }
     }
     
     func removeLastLine() {
@@ -217,6 +232,7 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
         scrollTextViewToBottom(historyView)
     }
     
+    // Ensures view is always scrolled to bottom
     func scrollTextViewToBottom(_ textView: UITextView) {
         if textView.text.count > 0 {
             let location = textView.text.count - 1
@@ -225,35 +241,7 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
         }
     }
     
-    @objc
-    func displayViewTap(sender:UITapGestureRecognizer) {
-        sumView = !sumView
-        showDisplay()
-        let message = sumView ? "Cumulative sum" : "Cumulative average"
-        showToast(controller: self, message: message, seconds: 0.5)
-    }
-    
-    // Display type average or sum
-    func showDisplay() {
-        displayView.text = sumView ? numberToDisplay(sum) : numberToDisplay(avg)
-    }
-    
-    @objc
-    func swipeLeft(sender: UISwipeGestureRecognizer) {
-        if sender.direction == .right {
-            if adder <= 80 {
-                adder = adder + 10
-            }
-        } else {
-            if adder >= 10 {
-                adder = adder - 10
-            }
-        }
-        if sender.state == .ended {
-            setButtonTitles()
-        }
-    }
-    
+    // Copy what is on the displayView
     func exportAsIs(_ text: String) {
         if text.count > 0 {
             let vc = UIActivityViewController(activityItems: [text], applicationActivities: [])
@@ -262,7 +250,6 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
                 UIActivityType.saveToCameraRoll
             ]
             present(vc, animated: true, completion: nil)
-            
         } else {
             showToast(controller: self, message: "No data to copy", seconds: 0.5)
         }
@@ -274,10 +261,8 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
         let date = Date()
         let dateString = dateFormatter.string(from: date)
         let fileName = dateString + ".csv"
-        
         let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName)
         
-        print(csvText)
         if csvText.count > csvHeader.count {
             do {
                 try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
@@ -301,7 +286,6 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate, GAD
 {
     override func layoutSubviews() {
         super.layoutSubviews()
-        
         updateCornerRadius()
     }
     
@@ -328,4 +312,3 @@ func showToast(controller: UIViewController, message: String, seconds: Double) {
         alert.dismiss(animated: true)
     }
 }
-
